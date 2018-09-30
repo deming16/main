bbf2d3f43ee3a586f98d85655d04b83e0b2bc2a1
hzf1174 <e0253687@MacBook-Pro.local>
2018-10-01 02:05:08 +0800

    Copy the Module model for implement

--- src/main/java/seedu/address/model/module/DuplicateModuleException.java
+++ src/main/java/seedu/address/model/module/DuplicateModuleException.java
@@ -1,4 +1,10 @@
-package seedu.address.model.module;
-
-public class DuplicateModuleException {
+package seedu.address.model.module.exceptions;
+/**
+ * Signals that the operation will result in duplicate Modules (Modules are considered duplicates if they
+ * have the same code).
+ */
+public class DuplicateModuleException extends RuntimeException {
+    public DuplicateModuleException() {
+        super("Operation would result in duplicate modules");
+    }
 }
