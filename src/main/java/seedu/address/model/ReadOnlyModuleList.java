932aa787890426af773ed353897bbe145b656d2f
hzf1174 <e0253687@MacBook-Pro.local>
2018-10-01 02:07:56 +0800

    Add search method for the database

--- src/main/java/seedu/address/model/ReadOnlyModuleList.java
+++ src/main/java/seedu/address/model/ReadOnlyModuleList.java
@@ -1,4 +1,13 @@
 package seedu.address.model;
-
-public class ReadOnlyModuleList {
+import javafx.collections.ObservableList;
+import seedu.address.model.module.Module;
+/**
+ * Unmodifiable view of a module list
+ */
+public interface ReadOnlyModuleList {
+    /**
+     * Returns an unmodifiable view of the persons list.
+     * This list will not contain any duplicate persons.
+     */
+    ObservableList<Module> getModuleList();
 }
